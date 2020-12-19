88d75849c3e3446a26e4b5d61fb88fe9b893827e
Robert Banaszak <robert.banaszak1997@gmail.com>
2020-12-19 12:44:42 +0100

    voting in multiselect polls

--- poll/service.go
+++ poll/service.go
@@ -10,6 +10,6 @@ type PollService interface {
 	GetActivePolls() (*[]Poll, error)
 	CreatePoll(poll *Poll, answers *[]Answer) error
 	GetAnswers(pollID gocql.UUID) (*[]Answer, error)
-	Vote(vote *Vote) error
+	Vote(poll *Poll, votes *[]Vote) error
 	GetResults(pollID gocql.UUID, dueTime time.Time) (*map[Answer]int, error)
 }
