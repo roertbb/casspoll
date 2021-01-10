7cd172ac40ffd4fa6f0b252b0b77ad86f0e97023
Robert Banaszak <robert.banaszak1997@gmail.com>
2021-01-10 17:35:29 +0100

    api client

---
+++ cmd/bot/api.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gocql/gocql"
+	"github.com/roertbb/casspoll/poll"
+)
+
+// TODO: customizable?
+const minAnwers = 3
+const maxAnswers = 9
+
+func createRandomPoll(address string) (*poll.Poll, error) {
+	type createPollDTO struct {
+		Title       string        `json:"title"`
+		Description string        `json:"description"`
+		PollType    poll.PollType `json:"type"`
+		DueTime     time.Time     `json:"dueTime"`
+		Answers     []string      `json:"answers"`
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	answersNo := rand.Intn(maxAnswers-minAnwers) + minAnwers
+	answers := []string{}
+	for i := 0; i < answersNo; i++ {
+		answers = append(answers, randString(30))
+	}
+
+	data := createPollDTO{
+		Title:       randString(20),
+		Description: randString(50),
+		PollType:    poll.PollType(rand.Intn(2) + 1),
+		// TODO: customizable?
+		DueTime: time.Now().Add(time.Second * time.Duration(60)),
+		Answers: answers,
+	}
+
+	marshBody, _ := json.Marshal(data)
+
+	resp, err := http.Post(address+"/polls", "application/json", bytes.NewBuffer(marshBody))
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	body := scanner.Text()
+
+	type response struct {
+		ID gocql.UUID `json:"id"`
+	}
+
+	responseData := response{}
+	err = json.Unmarshal([]byte(body), &responseData)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	p := poll.Poll{
+		ID:          responseData.ID,
+		Title:       data.Title,
+		Description: data.Description,
+		PollType:    data.PollType,
+		DueTime:     data.DueTime,
+	}
+
+	return &p, nil
+}
+
+func getActivePolls(address string) (*[]poll.Poll, error) {
+	resp, err := http.Get(address + "/polls")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	respBody := scanner.Text()
+
+	pollsData := []poll.Poll{}
+	err = json.Unmarshal([]byte(respBody), &pollsData)
+
+	return &pollsData, nil
+}
+
+func getAnswers(address string, pollData *poll.Poll) (*[]poll.Answer, error) {
+	resp, err := http.Get(address + "/polls/" + pollData.ID.String() + "/answers")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	respBody := scanner.Text()
+
+	answersData := []poll.Answer{}
+	err = json.Unmarshal([]byte(respBody), &answersData)
+
+	return &answersData, nil
+}
+
+func voteRandomlyForPoll(address string, pollData *poll.Poll, voterID gocql.UUID) (*[]gocql.UUID, error) {
+	type voteDTO struct {
+		VoterID gocql.UUID   `json:"voterId"`
+		Answers []gocql.UUID `json:"answers"`
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	resp, err := http.Get(address + "/polls/" + pollData.ID.String() + "/answers")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	respBody := scanner.Text()
+
+	answersData := []poll.Answer{}
+	err = json.Unmarshal([]byte(respBody), &answersData)
+
+	selectedAnswers := []gocql.UUID{}
+	if pollData.PollType == poll.MultipleChoice {
+		randID := rand.Intn(len(answersData))
+		selectedAnswers = append(selectedAnswers, answersData[randID].ID)
+	} else {
+		IDs := rand.Perm(len(answersData))
+		for _, idx := range IDs {
+			selectedAnswers = append(selectedAnswers, answersData[idx].ID)
+		}
+	}
+
+	body := voteDTO{VoterID: voterID, Answers: selectedAnswers}
+	marshBody, err := json.Marshal(body)
+
+	_, err = http.Post(address+"/polls/"+pollData.ID.String()+"/vote", "application/json", bytes.NewBuffer(marshBody))
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return &selectedAnswers, nil
+}
+
+func getPollResults(address string, pollData *poll.Poll) (*[]poll.Result, error) {
+	resp, err := http.Get(address + "/polls/" + pollData.ID.String() + "/results")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan()
+	respBody := scanner.Text()
+
+	resultsData := []poll.Result{}
+	err = json.Unmarshal([]byte(respBody), &resultsData)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return &resultsData, nil
+}
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,61 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/gocql/gocql"
-	"github.com/roertbb/casspoll/poll"
-	cass "github.com/roertbb/casspoll/repo/cassandra"
 )
 
 func main() {
-	addresses := strings.Split(os.Getenv("ADDRESS"), ",")
-	if len(addresses) == 1 && addresses[0] == "" {
-		log.Fatal("ADDRESS env variable not specified")
-		os.Exit(1)
-	}
-	keyspace := os.Getenv("KEYSPACE")
-	if keyspace == "" {
-		log.Fatal("KEYSPACE env variable not specified")
-		os.Exit(1)
-	}
+	// addresses := strings.Split(os.Getenv("ADDRESS"), ",")
+	// if len(addresses) == 1 && addresses[0] == "" {
+	// 	log.Fatal("ADDRESS env variable not specified")
+	// 	os.Exit(1)
+	// }
 
-	repo, _ := cass.NewCassandraRepo(addresses, keyspace)
-	service := poll.NewPollService(repo)
+	address := "http://127.0.0.1:8080"
 
-	pollID, _ := gocql.RandomUUID()
-	examplePoll := poll.Poll{
-		ID:          pollID,
-		Title:       "test",
-		Description: "test desc",
-		PollType:    poll.MultipleChoice,
-		DueTime:     time.Now().Add(time.Second * 10),
-	}
+	createRandomPoll(address)
 
-	service.CreatePoll(&examplePoll, &[]string{"1", "2", "3"})
-
-	polls, _ := service.GetActivePolls()
-	fmt.Println("---")
-	fmt.Println("active polls")
+	polls, _ := getActivePolls(address)
 	fmt.Println(polls)
 
-	pollAnswers, _ := service.GetAnswers(pollID)
-	fmt.Println("---")
-	fmt.Println("answers")
-	fmt.Println(pollAnswers)
+	voterUUID, _ := gocql.RandomUUID()
 
-	voterID, _ := gocql.RandomUUID()
-	service.Vote(pollID, &[]gocql.UUID{(*pollAnswers)[0].ID, (*pollAnswers)[1].ID}, voterID)
+	p := (*polls)[0]
 
-	voter2ID, _ := gocql.RandomUUID()
-	service.Vote(pollID, &[]gocql.UUID{(*pollAnswers)[0].ID}, voter2ID)
+	selectedAnswers, _ := voteRandomlyForPoll(address, &p, voterUUID)
+	fmt.Println(selectedAnswers)
 
-	results, _ := service.GetResults(examplePoll.ID)
-
-	fmt.Println("---")
-	fmt.Println("results")
-	fmt.Println(results)
+	res, _ := getPollResults(address, &p)
+	for _, r := range *res {
+		fmt.Println(r.Answer, r.VotesNo)
+	}
 }
